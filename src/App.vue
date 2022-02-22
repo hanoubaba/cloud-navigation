f253229d30199c84ad47d387976732002d89bded
hanouba <hanxiaogai@outlook.com>
2022-02-22 11:40:19 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <HelloWorld msg="极限收束模型" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+*{
+ margin: 0;
+    padding: 0;
+}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+   
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="bg">
+      <div class="item" @click="goUrl(0)"></div>
+      <div class="item" @click="goUrl(1)"></div>
+      <div class="item" @click="goUrl(2)"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    goUrl(n){
+      if(n == 0){
+        window.open('http://10.0.66.108:9090/')
+      }
+      if(n == 1){
+        window.open('http://10.0.66.108:8090/')
+      }
+      if(n == 2){
+        window.open('http://10.0.66.108:9099/')
+      }
+    }
+  },
+};
+</script>
+
+<style scoped>
+.bg {
+  width: 100%;
+  height: 1057px;
+  background-image: url("../assets/bg.png");
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.item {
+  /* border: 1px solid red; */
+  width: 382px;
+  height: 300px;
+  margin: 0 10px;
+  cursor: pointer;
+}
+</style>
